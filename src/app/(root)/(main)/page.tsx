dcdc89b91ddf4eecdc114ee6a766594d1120966a
Sanghyo Kevin Lee <101755500+SanghyoKLee@users.noreply.github.com>
2025-06-24 21:48:30 -0400

    fix(hero): removed UserStats

--- src/app/(root)/(main)/page.tsx
+++ src/app/(root)/(main)/page.tsx
@@ -2,14 +2,12 @@
 
 import { FeaturesSection } from "@/app/(root)/(main)/FeaturesSection";
 import { HeroSection } from "@/app/(root)/(main)/HeroSection";
-import UserStats from "@/components/UserStats";
 
 export default function HomePage() {
   return (
     <div className="w-full h-full max-w-4xl mx-auto space-y-6 sm:space-y-8 xl:space-y-8">
       <HeroSection />
       <FeaturesSection />
-      <UserStats></UserStats>
     </div>
   );
 }
